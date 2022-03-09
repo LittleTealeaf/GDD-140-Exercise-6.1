15ee37f0c34f682079181f1b46645ed10a40d2e1
Thomas Kwashnak <shintaomonk@gmail.com>
2022-03-08 21:21:37 -0500

    added label for actual height

--- sketch.js
+++ sketch.js
@@ -17,29 +17,29 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background('#5555FF');
+  fill('green');
+  noStroke();
+  rect(0,height,width,-20);
+  
   const item_width = width / data.length;
-
-
   const building_girth = 0.8;
   const max_height = height * 0.9;
-
   const offset = (1 - building_girth) / 2  * item_width;
-
-
-
-  const max = data.reduce((p,c) => Math.max(p,c.height),0);
-  
+  const max = data.reduce((p,c) => Math.max(p,c.height),0);  
 
   for(var i = 0; i < data.length; i++) {
     const val = data[i];
     const height_percentage = val.height / max;
     
+    stroke(1);
+    fill('grey');
     rect(item_width * i + offset, height,item_width * building_girth,-max_height * height_percentage);
 
     // text(val.name,item_width * i + offset + 20,height - 50);
     // text(val.location,item_width * i + offset + 20, height - 25);
     push();
+    fill('white');
     translate(item_width * (i + 1) - offset,height);
     rotate(-HALF_PI);
     translate(10,-10);
@@ -47,6 +47,14 @@ function draw() {
     text(val.name + ", " + val.location,0,0);
     pop();
 
+    push();
+    translate(item_width * i + offset,height - max_height * height_percentage);
+    
+    fill('#FFFFFF');
+    textSize(30);
+    text(val.height + " ft",0,0);
+
+    pop();
   }
 }
 
